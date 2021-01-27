53c90a5048b8faa8e58300b063874efda13273d8
chenshuo <c1094829085@qq.com>
2021-01-27 11:52:32 +0800

    black beautify

--- main.py
+++ main.py
@@ -18,22 +18,24 @@ if __name__ == "__main__":
     num_file_dict = get_num_file_dict()
     total = 0
     correct = 0
-    
+
     if int(input("Print 1 to Start: ")) == 1:
         while True:
-            num = random.randint(0,1000)
+            num = random.randint(0, 1000)
             total = total + 1
-            playsound("./Sounds_0to1000/"+str(num)+".mp3")
+            playsound("./Sounds_0to1000/" + str(num) + ".mp3")
             your_ans = input("Input your answer: (r to replay)")
-            
+
             while your_ans == "r":
                 playsound("./Sounds_0to1000/" + str(num) + ".mp3")
                 your_ans = input("Input your answer: (r to replay)")
-                
+
             if int(your_ans) == num:
                 correct = correct + 1
-                print(f"Correct! Correct {correct} / Total {total} = {correct/total * 100:.1f}%")
-        
+                print(
+                    f"Correct! Correct {correct} / Total {total} = {correct/total * 100:.1f}%"
+                )
+
             elif int(your_ans) is not num:
                 print(f"Wrong, Correct Ans:{num}")
                 while int(your_ans) != num:
