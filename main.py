b4648a4da561a3f93d95812244d9a08f0a2a0933
chenshuo <c1094829085@qq.com>
2021-02-01 18:57:32 +0800

    add detection of number

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ def get_num_file_dict():
     return d
 
 
+def check_num(input_content):
+    try:
+        input_num = int(input_content)
+    except ValueError:
+        print("Please Input Number")
+        return -1
+    else:
+        return input_num
+
 if __name__ == "__main__":
     num_file_dict = get_num_file_dict()
     total = 0
@@ -25,6 +34,10 @@ if __name__ == "__main__":
             total = total + 1
             playsound("./Sounds_0to1000/" + str(num) + ".mp3")
             your_ans = input("Input your answer: (r to replay)")
+            
+            while check_num(your_ans) == -1:
+                your_ans = input("Input your answer: (r to replay)")
+            
 
             while your_ans == "r":
                 playsound("./Sounds_0to1000/" + str(num) + ".mp3")
